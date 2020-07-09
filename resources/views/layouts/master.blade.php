9b984b7f29f30eeee6f61edf8d8091ba0d29d72f
Vitrio Wibisono <vitrio@noxus.co.id>
2020-07-09 15:46:37 +0700

    New view structure layouts (for master skeleton), partials (for inclusions e.g. header, footer, scripts, etc.) and pages (for the page e.g. dashboard, upload, user, etc.)

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @include('partials.header')
+    </head>
+    <body>
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>{{ config('app.name') }}</title>
+
+<!-- Scripts -->
+<script src="{{ asset('assets/vendors/paper/js/core/jquery.min.js') }}" defer></script>
+<script src="{{ asset('assets/vendors/paper/js/core/bootstrap.min.js') }}" defer></script>
+<script src="{{ asset('assets/vendors/paper/js/core/popper.min.js') }}" defer></script>
+<script src="{{ asset('assets/vendors/paper/js/paper-dashboard.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/app.js') }}"></script>
+
+<!-- Fonts -->
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,700,800" rel="styleshet">
+
+<!-- Styles -->
+<link href="{{ asset('assets/vendors/paper/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/vendors/paper/css/paper-dashboard.min.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
