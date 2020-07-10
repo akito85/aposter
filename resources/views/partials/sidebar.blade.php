5048867134f3e0a8b64c978365cff9d52c19c979
Vitrio Wibisono <vitrio@noxus.co.id>
2020-07-10 14:43:38 +0700

    Added overflow hidden for horizontal scrolling

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,21 +1,21 @@
 <div class="sidebar" data-color="white" data-active-color="danger">
     <div class="logo">
-    <a href="" class="simple-text logo-mini">
-        <div class="logo-image-small">
-        <!--
-            <img src="../assets/img/logo-small.png">
-        -->
-        </div>
-        <!-- <p>CT</p> -->
-    </a>
-    <a href="" class="simple-text logo-normal">
-        APOSTER
-        <!-- <div class="logo-image-big">
-        <img src="../assets/img/logo-big.png">
-        </div> -->
-    </a>
+        <a href="" class="simple-text logo-mini">
+            <div class="logo-image-small">
+            <!--
+                <img src="../assets/img/logo-small.png">
+            -->
+            </div>
+            <!-- <p>CT</p> -->
+        </a>
+        <a href="" class="simple-text logo-normal">
+            APOSTER
+            <!-- <div class="logo-image-big">
+            <img src="../assets/img/logo-big.png">
+            </div> -->
+        </a>
     </div>
-    <div class="sidebar-wrapper">
+    <div class="sidebar-wrapper" style="overflow:hidden;">
         <ul class="nav">
             @if(Route::currentRouteName() == 'dashboard')
             <li class="active">
