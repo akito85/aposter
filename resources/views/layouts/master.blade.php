70aad08df55163a9ebebcb9b78866aae461c5133
Vitrio Wibisono <vitrio@noxus.co.id>
2020-07-12 16:45:25 +0700

    Added logout and fixed master layout for login and register

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,12 +4,17 @@
         @include('partials.header')
     </head>
     <body>
+        @if (Auth::check())
         <main class="main-panel">
             <div class="content">
                 @include('partials.sidebar')
                 @yield('content')
             </div>
         </main>
+        @else
+        @yield('content')
+        @endif
+
         @yield('custom_js')
     </body>
 </html>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -37,6 +37,18 @@
                     <p>Upload</p>
                 </a>
             </li>
+            <li>
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                    <i class="nc-icon nc-user-run"></i>
+                    <p>{{ __('Logout') }}</p>
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </li>
         </ul>
     </div>
 </div>
