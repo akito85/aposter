26558e85bb6d2bebab81d60f53ecfcd9ab088240
Vitrio Wibisono <vitrio@noxus.co.id>
2020-07-13 11:12:25 +0700

    Added column organization_id in providers

---
+++ database/migrations/2020_07_12_094649_providers.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Providers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('providers', function (Blueprint $table) {
+            $table->id();
+            $table->string('organization_id')->nullable();
+            $table->string('organization_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
