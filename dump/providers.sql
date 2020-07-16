becb5988078d00650df46e4c312968417b325c23
Vitrio Wibisono <vitrio@noxus.co.id>
2020-07-16 11:03:44 +0700

    Database entry to help testing and debugging

---
+++ dump/providers.sql
@@ -0,0 +1,24 @@
+INSERT INTO public.providers (organization_id,organization_name,created_at,updated_at) VALUES 
+('18','Pusat Pendidikan dan Pelatihan Pengembangan Sumber Daya Manusia','2020-07-15 15:37:04.000','2020-07-15 15:37:04.000')
+,('19','Pusat Pendidikan dan Pelatihan Anggaran dan Perbendaharaan','2020-07-15 15:37:39.000','2020-07-15 15:37:39.000')
+,('20','Pusat Pendidikan dan Pelatihan Pajak','2020-07-15 16:14:34.000','2020-07-15 16:14:34.000')
+,('21','Pusat Pendidikan dan Pelatihan Bea dan Cukai','2020-07-15 16:38:59.000','2020-07-15 16:38:59.000')
+,('22','Pusat Pendidikan dan Pelatihan Kekayaan Negara dan Perimbangan Keuangan','2020-07-15 16:45:17.000','2020-07-15 16:45:17.000')
+,('17','Sekretariat Badan','2020-07-15 15:36:10.000','2020-07-15 15:36:10.000')
+,('23','Pusat Pendidikan dan Pelatihan Keuangan Umum','2020-07-15 16:45:57.000','2020-07-15 16:45:57.000')
+,('24','Politeknik Keuangan Negara STAN','2020-07-15 17:20:33.000','2020-07-15 17:20:33.000')
+,('25','Balai Diklat Keuangan Medan','2020-07-15 17:20:56.000','2020-07-15 17:20:56.000')
+,('26','Balai Diklat Keuangan Palembang','2020-07-15 17:21:16.000','2020-07-15 17:21:16.000')
+;
+INSERT INTO public.providers (organization_id,organization_name,created_at,updated_at) VALUES 
+('27','Balai Diklat Keuangan Yogyakarta','2020-07-15 17:21:35.000','2020-07-15 17:21:35.000')
+,('28','Balai Diklat Keuangan Malang','2020-07-15 17:21:52.000','2020-07-15 17:21:52.000')
+,('29','Balai Diklat Keuangan Balikpapan','2020-07-15 17:22:09.000','2020-07-15 17:22:09.000')
+,('30','Balai Diklat Keuangan Makassar','2020-07-15 17:22:26.000','2020-07-15 17:22:26.000')
+,('31','Balai Diklat Keuangan Cimahi','2020-07-15 17:22:40.000','2020-07-15 17:22:40.000')
+,('32','Balai Diklat Keuangan Manado','2020-07-15 17:23:01.000','2020-07-15 17:23:01.000')
+,('33','Balai Diklat Keuangan Pekanbaru','2020-07-15 17:23:18.000','2020-07-15 17:23:18.000')
+,('34','Balai Diklat Keuangan Pontianak','2020-07-15 17:23:33.000','2020-07-15 17:23:33.000')
+,('35','Balai Diklat Keuangan Denpasar','2020-07-15 17:23:51.000','2020-07-15 17:23:51.000')
+,('36','Balai Diklat Kepemimpinan','2020-07-15 17:24:40.000','2020-07-15 17:24:40.000')
+;
