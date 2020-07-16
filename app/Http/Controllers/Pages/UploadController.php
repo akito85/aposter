ab5b50c94ed9892c4de1604bfcfc4a278d57ff4e
Vitrio Wibisono <vitrio@noxus.co.id>
2020-07-16 12:36:40 +0700

    Refactored upload controller code, added new function for upload data participants

--- app/Http/Controllers/Pages/UploadController.php
+++ app/Http/Controllers/Pages/UploadController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers\Pages;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\UploadRequest;
+use App\Http\Requests\UploadParticipantsRequest;
+use App\Http\Requests\UploadReultsRequest;
 use App\Http\Controllers\Controller;
 use App\Imports\ParticipantsImport;
 use App\Imports\TrxTrainingsImport;
 use App\Models\UploadLogModel;
+use App\Helpers\CleanerHelper;
 use Maatwebsite\Excel\Facades\Excel;
 
 class UploadController extends Controller
 {
+    public $email;
+
     /**
      * Create a new controller instance.
      *
@@ -20,6 +24,7 @@ class UploadController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->email = auth()->user()->email;
     }
 
     /**
@@ -29,17 +34,55 @@ class UploadController extends Controller
      */
     public function index()
     {
+        return view('pages.upload', ['logs' => $this->listUploadLog()]);
+    }
+
+    public function fileUploadParticipants(UploadParticipantsRequest $request)
+    {
+        $cleaner = new CleanerHelper();
+
+        $idata = Excel::toArray(new ParticipantsImport, $file);
+        $title = $idata[0][1][0];
+        $ta = explode(' ', $cleaner->removeWhiteSpace($idata[0][2][0]));
+        $ta = $ta[2];
+
+        Excel::Import(new ParticipantsImport, $file);
+        Excel::Import(new TrxTrainingsImport, $file);
+
+        $this->saveUploadLog($this->changeFileName('data_participants'), $title, $ta, $this->email);
+
         return view('pages.upload', ['logs' => $this->logUploadList()]);
     }
 
-    public function fileUploadParticipants(UploadRequest $request)
-    {
-        // get logged user
-        $user = auth()->user();
-        $id = $user->id;
-        $name = $user->name;
-        $email = $user->email;
+    public function fileUploadResults(UploadResultsRequest $request) {
 
+        $this->saveUploadLog($this->changeFileName('data_requests'), '', '', $this->email);
+
+        return view('pages.upload', ['logs' => $this->listUploadLog()]);
+    }
+
+    private function listUploadLog()
+    {
+        return UploadLogModel::select('*')
+                    ->limit(11)
+                    ->orderBy('created_at', 'desc')
+                    ->get();
+    }
+
+    private function saveUploadLog($file, $training_name = '', $year = '', $email)
+    {
+        UploadLogModel::create(
+            [
+                'file_name' => $file,
+                'training_name' => $title,
+                'training_year' => $year,
+                'uploader_name' => $email
+            ]
+        );
+    }
+
+    private function changeFileName($file)
+    {
         // upload timestamp for filename
         $ts = date('Y-m-d H:i:s');
         $ts = \Carbon\Carbon::createFromFormat(
@@ -47,33 +90,9 @@ class UploadController extends Controller
             $ts
         )->format('d-m-Y_H.i.s');
 
-        $file = $request->file('data');
+        $file = $request->file($file);
         $fn = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
 
-        $idata = Excel::toArray(new ParticipantsImport, $file);
-        $title = $idata[0][1][0];
-        $ta = $idata[0][2][0];
-
-        Excel::Import(new ParticipantsImport, $file);
-
-        Excel::Import(new TrxTrainingsImport, $file);
-
-        UploadLogModel::create(
-            [
-                'file_name' => $fn,
-                'training_name' => $title . ' ' . $ta,
-                'uploader_name' => $email
-            ]
-        );
-
-        return view('pages.upload', ['logs' => $this->logUploadList()]);
-    }
-
-    private function logUploadList()
-    {
-        return UploadLogModel::select('*')
-                    ->limit(11)
-                    ->orderBy('created_at', 'desc')
-                    ->get();
+        return $fn;
     }
 }
