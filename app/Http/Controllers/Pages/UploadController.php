4ab754f9210f23901865d2fcb64103cb32961fd7
Vitrio Wibisono <vitrio@noxus.co.id>
2020-07-20 11:50:38 +0700

    Test for php failed

--- app/Http/Controllers/Pages/UploadController.php
+++ app/Http/Controllers/Pages/UploadController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use App\Imports\ParticipantsImport;
 use App\Imports\TrxTrainingsImport;
 use App\Models\UploadLogModel;
+use App\Models\TrxResultsModel;
 use App\Helpers\CleanerHelper;
 use Maatwebsite\Excel\Facades\Excel;
 use PHPHtmlParser\Dom;
@@ -59,7 +60,7 @@ class UploadController extends Controller
 
     public function fileUploadResults(UploadResultsRequest $request) {
         $rawfile = $request->file('data_results');
-        $tempfile = fopen('temp/temp.txt', 'w') or die('Unable to open file!');
+        // $tempfile = fopen('temp/temp.txt', 'w') or die('Unable to open file!');
         $html = fopen($rawfile, 'r') or die('Unable to open file!');
 
         /* this code use python to do the html parse
@@ -92,57 +93,65 @@ class UploadController extends Controller
         foreach ($rows as $row) {
             $cols = $row->getElementsByTagName('td');
 
-            TrxResultsModel::create(
-                [
-                    'training_id' => is_object($cols->item(1)) ? $cols->item(1)->nodeValue : '',
-                    'training_name' => is_object($cols->item(2)) ? $cols->item(2)->nodeValue : '',
-                    'start' => is_object($cols->item(3)) ? $cols->item(3)->nodeValue : '',
-                    'end' => is_object($cols->item(4)) ? $cols->item(4)->nodeValue : '',
-                    'hours' => is_object($cols->item(5)) ? $cols->item(5)->nodeValue : '',
-                    'days' => is_object($cols->item(6)) ? $cols->item(6)->nodeValue : '',
-                    'organization_name' => is_object($cols->item(7)) ? $cols->item(7)->nodeValue : '',
-                    'type' => is_object($cols->item(8)) ? $cols->item(8)->nodeValue : '',
-                    'cost' => is_object($cols->item(9)) ? $cols->item(9)->nodeValue : '',
-                    'cost_detail' => is_object($cols->item(10)) ? $cols->item(10)->nodeValue : '',
-                    'elearning' => is_object($cols->item(11)) ? $cols->item(11)->nodeValue : '',
-                    'test' => is_object($cols->item(12)) ? $cols->item(12)->nodeValue : '',
-                    'class' => is_object($cols->item(13)) ? $cols->item(13)->nodeValue : '',
+            if(is_object($cols->item(14))) {
+               echo $cols->item(14)->nodeValue . "<br>";
+            }
 
-                    'id' => is_object($cols->item(14)) ? $cols->item(14)->nodeValue : '',
-                    'name' => is_object($cols->item(15)) ? $cols->item(15)->nodeValue : '',
-                    'nip' => is_object($cols->item(16)) ? $cols->item(16)->nodeValue : '',
-                    'nrp_nik' => is_object($cols->item(17)) ? $cols->item(17)->nodeValue : '',
-                    'rank_class' => is_object($cols->item(18)) ? $cols->item(18)->nodeValue : '',
-                    'born' => is_object($cols->item(19)) ? $cols->item(19)->nodeValue : '',
-                    'birthday' => is_object($cols->item(20)) ? $cols->item(20)->nodeValue : '',
-                    'gender' => is_object($cols->item(21)) ? $cols->item(21)->nodeValue : '',
-                    'phone' => is_object($cols->item(22)) ? $cols->item(22)->nodeValue : '',
-                    'email' => is_object($cols->item(23)) ? $cols->item(23)->nodeValue : '',
-                    'office_address' => is_object($cols->item(24)) ? $cols->item(24)->nodeValue : '',
-                    'education' => is_object($cols->item(25)) ? $cols->item(25)->nodeValue : '',
-                    'education_desc' => is_object($cols->item(26)) ? $cols->item(26)->nodeValue : '',
-                    'position' => is_object($cols->item(27)) ? $cols->item(27)->nodeValue : '',
-                    'position_desc' => is_object($cols->item(28)) ? $cols->item(28)->nodeValue : '',
-                    'married' => is_object($cols->item(29)) ? $cols->item(29)->nodeValue : '',
-                    'religion' => is_object($cols->item(30)) ? $cols->item(30)->nodeValue : '',
-                    'main_unit' => is_object($cols->item(31)) ? $cols->item(31)->nodeValue : '',
-                    'eselon2' => is_object($cols->item(32)) ? $cols->item(32)->nodeValue : '',
-                    'eselon3' => is_object($cols->item(33)) ? $cols->item(33)->nodeValue : '',
-                    'eselon4' => is_object($cols->item(34)) ? $cols->item(34)->nodeValue : '',
-                    'satker' => is_object($cols->item(35)) ? $cols->item(35)->nodeValue : '',
-                    'test' => is_object($cols->item(36)) ? $cols->item(36)->nodeValue : '',
-                    'graduate_status' => is_object($cols->item(37)) ? $cols->item(37)->nodeValue : '',
-                    'activity' => is_object($cols->item(38)) ? $cols->item(38)->nodeValue : '',
-                    'presence' => is_object($cols->item(39)) ? $cols->item(39)->nodeValue : '',
-                    'pre_test' => is_object($cols->item(40)) ? $cols->item(40)->nodeValue : '',
-                    'post_test' => is_object($cols->item(41)) ? $cols->item(41)->nodeValue : '',
+            // $data = [
+            //     'training_id' => is_object($cols->item(1)) ? $cols->item(1)->nodeValue : NULL,
+            //     'training_name' => is_object($cols->item(2)) ? $cols->item(2)->nodeValue : NULL,
+            //     'start' => is_object($cols->item(3)) ? $cols->item(3)->nodeValue : NULL,
+            //     'end' => is_object($cols->item(4)) ? $cols->item(4)->nodeValue : NULL,
+            //     'hours' => is_object($cols->item(5)) ? $cols->item(5)->nodeValue : NULL,
+            //     'days' => is_object($cols->item(6)) ? $cols->item(6)->nodeValue : NULL,
+            //     'organization_name' => is_object($cols->item(7)) ? $cols->item(7)->nodeValue : NULL,
+            //     'type' => is_object($cols->item(8)) ? $cols->item(8)->nodeValue : NULL,
+            //     'cost' => is_object($cols->item(9)) ? $cols->item(9)->nodeValue : NULL,
+            //     'cost_detail' => is_object($cols->item(10)) ? $cols->item(10)->nodeValue : NULL,
+            //     'elearning' => is_object($cols->item(11)) ? $cols->item(11)->nodeValue : NULL,
+            //     'type_test' => is_object($cols->item(12)) ? $cols->item(12)->nodeValue : NULL,
+            //     'class' => is_object($cols->item(13)) ? $cols->item(13)->nodeValue : NULL,
 
-                    'number' => is_object($cols->item(42)) ? $cols->item(42)->nodeValue : '',
-                    'date' => is_object($cols->item(43)) ? $cols->item(43)->nodeValue : '',
-                    'execution_value' => is_object($cols->item(44)) ? $cols->item(44)->nodeValue : '',
-                    'trainer_value' => is_object($cols->item(45)) ? $cols->item(45)->nodeValue : ''
-                ]
-            );
+            //     'student_id' => is_object($cols->item(14)) ? $cols->item(14)->nodeValue : NULL,
+            //     'name' => is_object($cols->item(15)) ? $cols->item(15)->nodeValue : NULL,
+            //     'nip' => is_object($cols->item(16)) ? $cols->item(16)->nodeValue : NULL,
+            //     'nrp_nik' => is_object($cols->item(17)) ? $cols->item(17)->nodeValue : NULL,
+            //     'rank_class' => is_object($cols->item(18)) ? $cols->item(18)->nodeValue : NULL,
+            //     'born' => is_object($cols->item(19)) ? $cols->item(19)->nodeValue : NULL,
+            //     'birthday' => is_object($cols->item(20)) ? $cols->item(20)->nodeValue : NULL,
+            //     'gender' => is_object($cols->item(21)) ? $cols->item(21)->nodeValue : NULL,
+            //     'phone' => is_object($cols->item(22)) ? $cols->item(22)->nodeValue : NULL,
+            //     'email' => is_object($cols->item(23)) ? $cols->item(23)->nodeValue : NULL,
+            //     'office_address' => is_object($cols->item(24)) ? $cols->item(24)->nodeValue : NULL,
+            //     'education' => is_object($cols->item(25)) ? $cols->item(25)->nodeValue : NULL,
+            //     'education_desc' => is_object($cols->item(26)) ? $cols->item(26)->nodeValue : NULL,
+            //     'position' => is_object($cols->item(27)) ? $cols->item(27)->nodeValue : NULL,
+            //     'position_desc' => is_object($cols->item(28)) ? $cols->item(28)->nodeValue : NULL,
+            //     'married' => is_object($cols->item(29)) ? $cols->item(29)->nodeValue : NULL,
+            //     'religion' => is_object($cols->item(30)) ? $cols->item(30)->nodeValue : NULL,
+            //     'main_unit' => is_object($cols->item(31)) ? $cols->item(31)->nodeValue : NULL,
+            //     'eselon2' => is_object($cols->item(32)) ? $cols->item(32)->nodeValue : NULL,
+            //     'eselon3' => is_object($cols->item(33)) ? $cols->item(33)->nodeValue : NULL,
+            //     'eselon4' => is_object($cols->item(34)) ? $cols->item(34)->nodeValue : NULL,
+            //     'satker' => is_object($cols->item(35)) ? $cols->item(35)->nodeValue : NULL,
+            //     'test_result' => is_object($cols->item(36)) ? $cols->item(36)->nodeValue : NULL,
+            //     'graduate_status' => is_object($cols->item(37)) ? $cols->item(37)->nodeValue : NULL,
+            //     'activity' => is_object($cols->item(38)) ? $cols->item(38)->nodeValue : NULL,
+            //     'presence' => is_object($cols->item(39)) ? $cols->item(39)->nodeValue : NULL,
+            //     'pre_test' => is_object($cols->item(40)) ? $cols->item(40)->nodeValue : NULL,
+            //     'post_test' => is_object($cols->item(41)) ? $cols->item(41)->nodeValue : NULL,
+
+            //     'number' => is_object($cols->item(42)) ? $cols->item(42)->nodeValue : NULL,
+            //     'date' => is_object($cols->item(43)) ? $cols->item(43)->nodeValue : NULL,
+            //     'execution_value' => is_object($cols->item(44)) ?  $cols->item(44)->nodeValue : NULL,
+            //     'trainer_value' => is_object($cols->item(45)) ? $cols->item(45)->nodeValue : NULL
+            // ];
+
+            // TrxResultsModel::create(
+            //     [
+            //         $data
+            //     ]
+            // );
 
          }
 
