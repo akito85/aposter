ec3ad4f9717202407b5785d96afcbb5405b113eb
Vitrio Wibisono <vitrio@noxus.co.id>
2020-07-20 10:03:26 +0700

    Added dump of user data

---
+++ dump/users.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users (name,email,email_verified_at,"password",remember_token,created_at,updated_at) VALUES 
+('akito','akito.evol@gmail.com',NULL,'$2y$10$e2FWRqOuyTHTYTa5kdgdquuY4I0oQGeFWRQxE8pUuyTp5SEBMQcsG','YSycIPBqNPwuioPwJh18FFVbWexzSi1lojsJREq9pvVCo6gOsbTd90YYDYTl','2020-07-15 04:22:03.000','2020-07-15 04:22:03.000')
+;
