8699a11928d6b7c87e7d6979d71f372c504c3fa2
Vitrio Wibisono <vitrio@noxus.co.id>
2020-07-23 13:42:04 +0700

    All charts in the dashboard

--- app/Http/Controllers/Pages/DashboardController.php
+++ app/Http/Controllers/Pages/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Pages;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\TrxResultsModel;
 
 class DashboardController extends Controller
 {
@@ -22,8 +23,169 @@ class DashboardController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request, $trName = NULL)
     {
-        return view('pages.dashboard');
+        $age = $this->getAggregateAge($trName);
+        $echelon = $this->getAggregateEchelon($trName);
+        $gender = $this->omniCount(
+                    $this->omniQuery('gender', 'training_name', 'nip', 'like', $trName), 'gender');
+        $rankClass = $this->omniCount(
+                        $this->omniQuery('rank_class', 'training_name', 'nip', 'like', $trName), 'rank_class');
+        $education = $this->omniCount(
+                        $this->omniQuery('education', 'training_name', 'nip', 'like', $trName), 'education');
+        $pass = $this->omniCount(
+            $this->omniQuery('graduate_status', 'training_name', 'nip', 'like', $trName), 'graduate_status');
+
+        $data = [
+            'training' => $trName,
+            'gender' => json_encode($gender),
+            'age' => json_encode($age),
+            'rc' => json_encode($rankClass),
+            'education' => json_encode($education),
+            'echelon' => json_encode($echelon),
+            'pass' => json_encode($pass)
+        ];
+
+        return view('pages.dashboard', $data);
+    }
+
+    private function getAggregateAge($trName)
+    {
+        // resulting row of objects
+        $query = $this->omniQuery('birthday', 'training_name', 'nip', 'like', $trName);
+
+        $now = time();
+
+        foreach ($query as $row) {
+            $age = date('Y', $now) - date('Y', strtotime($row->birthday));
+            if($age <= 20) {
+                $ages['l1'][] = $age;
+            } elseif ($age >= 21 and $age <= 30) {
+                $ages['l2'][] = $age;
+            } elseif ($age >= 31 and $age <= 40) {
+                $ages['l3'][] = $age;
+            } elseif ($age >= 41 and $age <= 50) {
+                $ages['l4'][] = $age;
+            } elseif ($age >= 51 and $age <= 60) {
+                $ages['l5'][] = $age;
+            }
+        }
+
+        $tier1 = !empty($ages['l1']) ? collect($ages['l1']) : 0;
+        $tier1 = !empty($tier1) ? $tier1->count() : 0;
+        $tier2 = !empty($ages['l2']) ? collect($ages['l2']) : 0;
+        $tier2 = !empty($tier2) ? $tier2->count() : 0;
+        $tier3 = !empty($ages['l3']) ? collect($ages['l3']) : 0;
+        $tier3 = !empty($tier3) ? $tier3->count() : 0;
+        $tier4 = !empty($ages['l4']) ? collect($ages['l4']) : 0;
+        $tier4 = !empty($tier4) ? $tier4->count() : 0;
+        $tier5 = !empty($ages['l5']) ? collect($ages['l5']) : 0;
+        $tier5 = !empty($tier5) ? $tier5->count() : 0;
+
+        $tier = [
+            'T1' => $tier1,
+            'T2' => $tier2,
+            'T3' => $tier3,
+            'T4' => $tier4,
+            'T5' => $tier5,
+        ];
+
+        return $tier;
+    }
+
+    private function getAggregateEchelon($trName)
+    {
+        // resulting row of objects
+        $query = $this->omniQuery('position_desc', 'training_name', 'nip', 'like', $trName);
+
+        // group echelon
+        foreach($query as $row) {
+            $position = $row->position_desc;
+
+            // pattern echelons
+            $p1 = '/dirjen|direkotrat jenderal|sekdir|sekretaris direktorat|kaban|kepala badan/i';
+            $p2 = '/kepala wilayah|kepala pusat/i';
+            $p3 = '/kabid|kepala bidag|kabag|kepala bagian/i';
+            $p4 = '/kasubbid|kepala subbidang|kasubbag|kepala subbagian|kasi|kepala seksi|kepala subdirektorat|kasubdir|kasubdit|kepala subdir|kepala subdit/i';
+            $p5 = '/^pelaksana|penata|penyaji|pengatur|^pengolah/i';
+
+            if(preg_match($p1, $position)) {
+                $e['Eselon I'][] = $position;
+            } elseif(preg_match($p2, $position)) {
+                $e['Eselon II'][] = $position;
+            } elseif(preg_match($p3, $position)) {
+                $e['Eselon III'][] = $position;
+            } elseif(preg_match($p4, $position)) {
+                $e['Eselon IV'][] = $position;
+            } elseif(preg_match($p5, $position)) {
+                $e['Pelaksana'][] = $position;
+            } else {
+                $e['Fungsional'][] = $position;
+            }
+        }
+
+        // count array
+        if(!empty($e['Eselon I'])) {
+            $c['Eselon I'] = collect($e['Eselon I'])->count();
+        } else {
+            $c['Eselon I'] = 0;
+        }
+        if(!empty($e['Eselon II'])) {
+            $c['Eselon II'] = collect($e['Eselon II'])->count();
+        } else {
+            $c['Eselon II'] = 0;
+        }
+        if(!empty($e['Eselon III'])) {
+            $c['Eselon III'] = collect($e['Eselon III'])->count();
+        } else {
+            $c['Eselon III'] = 0;
+        }
+        if(!empty($e['Eselon IV'])) {
+            $c['Eselon IV'] = collect($e['Eselon IV'])->count();
+        } else {
+            $c['Eselon IV'] = 0;
+        }
+        if(!empty($e['Pelaksana'])) {
+            $c['Pelaksana'] = collect($e['Pelaksana'])->count();
+        } else {
+            $c['Pelaksana'] = 0;
+        }
+
+        $c['Fungsional'] = collect($e['Fungsional'])->count();
+
+        return $c;
+    }
+
+    private function omniQuery($selectField, $whereField, $distinctField = NULL, $queryOperator, $queryString)
+    {
+        // resulting row of objects
+        if(!empty($queryString)) {
+            $query = TrxResultsModel::select($selectField)
+                        ->where($whereField, $queryOperator, $queryString)
+                        ->get();
+        } else {
+            $query = TrxResultsModel::select($selectField, $distinctField)
+                        //->distinct($distinctField)
+                        ->get();
+        }
+
+        return $query;
+    }
+
+    private function omniCount($queryResult = NULL, $selectField)
+    {
+        if(!empty($queryResult)) {
+            foreach ($queryResult as $row) {
+                $data[] = $row->$selectField;
+            }
+
+            $collection = collect($data);
+
+            $counted = $collection->countBy();
+
+            return $counted->all();
+        }
+
+        return false;
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,12 +1,10 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-lg-4 col-md-12 col-sm-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -14,10 +12,461 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <canvas id="chart-gender"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-12 col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <canvas id="chart-age"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-12 col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <canvas id="chart-rc"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-4 col-md-12 col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <canvas id="chart-education"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-12 col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <canvas id="chart-echelon"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-12 col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <canvas id="chart-pass"></canvas>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('custom_js')
+<script>
+  $(function(){
+    /** can be refactored and simplified **/
+
+      // get the chart canvas
+      var cData = JSON.parse(`<?php echo $gender; ?>`);
+      var ctx = $("#chart-gender");
+
+      // chart data
+      var data = {
+        labels: ["PRIA", "WANITA"],
+        datasets: [
+          {
+            label: "Data Jenis Kelamain",
+            data: [cData.PRIA, cData.WANITA],
+            backgroundColor: [
+              "#DEB887",
+              "#A9A9A9",
+            ],
+            borderColor: [
+              "#CDA776",
+              "#989898",
+            ],
+            borderWidth: [1, 1]
+          }
+        ]
+      };
+
+      //options
+      var options = {
+        responsive: true,
+        title: {
+          display: true,
+          position: "top",
+          text: "{{ $training }}",
+          fontSize: 17,
+          fontColor: "#111"
+        },
+        legend: {
+          display: true,
+          position: "bottom",
+          labels: {
+            fontColor: "#333",
+            fontSize: 16
+          }
+        },
+        plugins: {
+          datalabels: {
+            anchor: "center",
+            clamp: true
+          }
+        }
+      };
+
+      // create Pie Chart class object
+      var chart_gender = new Chart(ctx, {
+        type: "bar",
+        plugins: [ChartDataLabels],
+        data: data,
+        options: options
+      });
+
+      // get the chart canvas
+      var cData = JSON.parse(`<?php echo $age; ?>`);
+      var ctx = $("#chart-age");
+
+      // chart data
+      var data = {
+        labels: ['1 - 20', '21 - 30', '31 - 40', '41 - 50', '51 - 60'],
+        datasets: [
+          {
+            label: "Umur Peserta",
+            data: [cData.T1, cData.T2, cData.T3, cData.T4, cData.T5],
+            backgroundColor: [
+              "#DEB887",
+              "#A9A9A9",
+            ],
+            borderColor: [
+              "#CDA776",
+              "#989898",
+            ],
+            borderWidth: [1, 1]
+          }
+        ]
+      };
+
+      //options
+      var options = {
+        responsive: true,
+        title: {
+          display: true,
+          position: "top",
+          text: "{{ $training }}",
+          fontSize: 17,
+          fontColor: "#111"
+        },
+        legend: {
+          display: true,
+          position: "bottom",
+          labels: {
+            fontColor: "#333",
+            fontSize: 16
+          }
+        },
+        plugins: {
+          datalabels: {
+            anchor: "center",
+            clamp: true
+          }
+        }
+      };
+
+      // create Pie Chart class object
+      var chart_age = new Chart(ctx, {
+        type: "bar",
+        plugins: [ChartDataLabels],
+        data: data,
+        options: options
+      });
+
+      // get the chart canvas
+      var cData = JSON.parse(`<?php echo $rc; ?>`);
+      var ctx = $("#chart-rc");
+
+      var labels = Object.getOwnPropertyNames(cData).sort();
+      var values = [];
+
+      for (let i = 0; i < labels.length; i++) {
+        values.push(cData[labels[i]]);
+      }
+
+      // chart data
+      var data = {
+        labels: labels,
+        datasets: [
+          {
+            label: "Pangkat / Golongan Peserta",
+            data: values,
+            backgroundColor: [
+              "#DEB887",
+              "#A9A9A9",
+            ],
+            borderColor: [
+              "#CDA776",
+              "#989898",
+            ],
+            borderWidth: [1, 1]
+          }
+        ]
+      };
+
+      //options
+      var options = {
+        responsive: true,
+        title: {
+          display: true,
+          position: "top",
+          text: "{{ $training }}",
+          fontSize: 17,
+          fontColor: "#111"
+        },
+        legend: {
+          display: true,
+          position: "bottom",
+          labels: {
+            fontColor: "#333",
+            fontSize: 16
+          }
+        },
+        plugins: {
+          datalabels: {
+            anchor: "center",
+            clamp: true
+          }
+        }
+      };
+
+      // create Pie Chart class object
+      var chart_rc = new Chart(ctx, {
+        type: "horizontalBar",
+        plugins: [ChartDataLabels],
+        data: data,
+        options: options
+      });
+
+      // get the chart canvas
+      var cData = JSON.parse(`<?php echo $education; ?>`);
+      var ctx = $("#chart-education");
+
+      var labels = Object.getOwnPropertyNames(cData).sort();
+      var values = [];
+
+      for (let i = 0; i < labels.length; i++) {
+        values.push(cData[labels[i]]);
+      }
+
+      // chart data
+      var data = {
+        labels: labels,
+        datasets: [
+          {
+            label: "Pendidikan",
+            data: values,
+            backgroundColor: [
+              "#DEB887",
+              "#A9A9A9",
+            ],
+            borderColor: [
+              "#CDA776",
+              "#989898",
+            ],
+            borderWidth: [1, 1]
+          }
+        ]
+      };
+
+      //options
+      var options = {
+        responsive: true,
+        title: {
+          display: true,
+          position: "top",
+          text: "{{ $training }}",
+          fontSize: 17,
+          fontColor: "#111"
+        },
+        legend: {
+          display: true,
+          position: "bottom",
+          labels: {
+            fontColor: "#333",
+            fontSize: 16
+          }
+        },
+        plugins: {
+          datalabels: {
+            anchor: "center",
+            clamp: true
+          }
+        }
+      };
+
+      // create Pie Chart class object
+      var chart_education = new Chart(ctx, {
+        type: "bar",
+        plugins: [ChartDataLabels],
+        data: data,
+        options: options
+      });
+
+      // get the chart canvas
+      var cData = JSON.parse(`<?php echo $echelon; ?>`);
+      var ctx = $("#chart-echelon");
+
+      var labels = Object.getOwnPropertyNames(cData).sort();
+      var values = [];
+
+      for (let i = 0; i < labels.length; i++) {
+        values.push(cData[labels[i]]);
+      }
+
+      // chart data
+      var data = {
+        labels: labels,
+        datasets: [
+          {
+            label: "Eselon dan non-eselon",
+            data: values,
+            backgroundColor: [
+              "#DEB887",
+              "#A9A9A9",
+            ],
+            borderColor: [
+              "#CDA776",
+              "#989898",
+            ],
+            borderWidth: [1, 1]
+          }
+        ]
+      };
+
+      //options
+      var options = {
+        responsive: true,
+        title: {
+          display: true,
+          position: "top",
+          text: "{{ $training }}",
+          fontSize: 17,
+          fontColor: "#111"
+        },
+        legend: {
+          display: true,
+          position: "bottom",
+          labels: {
+            fontColor: "#333",
+            fontSize: 16
+          }
+        },
+        plugins: {
+          datalabels: {
+            anchor: "center",
+            clamp: true
+          }
+        }
+      };
+
+      // create Pie Chart class object
+      var chart_echelon = new Chart(ctx, {
+        type: "horizontalBar",
+        plugins: [ChartDataLabels],
+        data: data,
+        options: options
+      });
+
+      // get the chart canvas
+      var cData = JSON.parse(`<?php echo $pass; ?>`);
+      var ctx = $("#chart-pass");
+
+      var labels = Object.getOwnPropertyNames(cData).sort();
+      var values = [];
+
+      for (let i = 0; i < labels.length; i++) {
+        values.push(cData[labels[i]]);
+      }
+
+      // chart data
+      var data = {
+        labels: labels,
+        datasets: [
+          {
+            label: "Kelulusan",
+            data: values,
+            backgroundColor: [
+              "#DEB887",
+              "#A9A9A9",
+            ],
+            borderColor: [
+              "#CDA776",
+              "#989898",
+            ],
+            borderWidth: [1, 1]
+          }
+        ]
+      };
+
+      //options
+      var options = {
+        responsive: true,
+        title: {
+          display: true,
+          position: "top",
+          text: "{{ $training }}",
+          fontSize: 17,
+          fontColor: "#111"
+        },
+        legend: {
+          display: true,
+          position: "bottom",
+          labels: {
+            fontColor: "#333",
+            fontSize: 16
+          }
+        },
+        plugins: {
+          datalabels: {
+            anchor: "center",
+            clamp: true
+          }
+        }
+      };
+
+      // create Pie Chart class object
+      var chart_pass = new Chart(ctx, {
+        type: "horizontalBar",
+        plugins: [ChartDataLabels],
+        data: data,
+        options: options
+      });
+  });
+</script>
 @endsection
