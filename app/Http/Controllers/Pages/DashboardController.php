4c0e2ce3c158aee4ee55bc83a3fce659caff8aab
Vitrio Wibisono <vitrio@noxus.co.id>
2020-08-06 18:02:20 +0700

    Predefined variable data as null

--- app/Http/Controllers/Pages/DashboardController.php
+++ app/Http/Controllers/Pages/DashboardController.php
@@ -171,6 +171,8 @@ class DashboardController extends Controller
 
     private function omniCount($queryResult = NULL, $selectField)
     {
+        $data = NULL;
+
         if(!empty($queryResult)) {
             foreach ($queryResult as $row) {
                 $data[] = $row->$selectField;
