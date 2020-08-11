531d8c4946e56e4c99feef0b9a8c2764e1ede6e0
Vitrio Wibisono <vitrio@noxus.co.id>
2020-08-11 14:32:59 +0700

    Updated to secure assets url

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,20 +7,20 @@
 <title>{{ config('app.name') }}</title>
 
 <!-- Scripts -->
-<script src="{{ asset('assets/vendors/paper/js/core/jquery.min.js') }}"></script>
-<script src="{{ asset('assets/vendors/paper/js/core/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/vendors/paper/js/core/popper.min.js') }}"></script>
-<script src="{{ asset('assets/vendors/paper/js/plugins/chartjs.min.js') }}"></script>
-<script src="{{ asset('assets/vendors/paper/js/plugins/chartjs-plugin-datalabels.min.js') }}"></script>
-<script src="{{ asset('assets/vendors/paper/js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
-<script src="{{ asset('assets/vendors/select2/js/select2.min.js') }}"></script>
+<script src="{{ asset('secure_assets/vendors/paper/js/core/jquery.min.js') }}"></script>
+<script src="{{ asset('secure_assets/vendors/paper/js/core/bootstrap.min.js') }}"></script>
+<script src="{{ asset('secure_assets/vendors/paper/js/core/popper.min.js') }}"></script>
+<script src="{{ asset('secure_assets/vendors/paper/js/plugins/chartjs.min.js') }}"></script>
+<script src="{{ asset('secure_assets/vendors/paper/js/plugins/chartjs-plugin-datalabels.min.js') }}"></script>
+<script src="{{ asset('secure_assets/vendors/paper/js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
+<script src="{{ asset('secure_assets/vendors/select2/js/select2.min.js') }}"></script>
 
 <!-- Fonts -->
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,700,800" rel="styleshet">
 
 <!-- Styles -->
-<link href="{{ asset('assets/vendors/paper/css/bootstrap.min.css') }}" rel="stylesheet">
-<link href="{{ asset('assets/vendors/select2/css/select2.min.css') }}" rel="stylesheet">
-<link href="{{ asset('assets/vendors/paper/css/paper-dashboard.min.css') }}" rel="stylesheet">
-<link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('secure_assets/vendors/paper/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('secure_assets/vendors/select2/css/select2.min.css') }}" rel="stylesheet">
+<link href="{{ asset('secure_assets/vendors/paper/css/paper-dashboard.min.css') }}" rel="stylesheet">
+<link href="{{ asset('secure_assets/css/app.css') }}" rel="stylesheet">
