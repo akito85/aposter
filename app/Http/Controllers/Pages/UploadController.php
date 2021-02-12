094d12b8a519a3f99aaac80aea6d2ef723ce8781
Vitrio Wibisono <vitrio@noxus.co.id>
2021-02-12 14:23:29 +0700

    Changed web excel to csv uploader

--- app/Http/Controllers/Pages/UploadController.php
+++ app/Http/Controllers/Pages/UploadController.php
@@ -3,16 +3,13 @@
 namespace App\Http\Controllers\Pages;
 
 use Illuminate\Http\Request;
+use League\Csv\Reader;
 use App\Http\Requests\UploadParticipantsRequest;
 use App\Http\Requests\UploadResultsRequest;
 use App\Http\Controllers\Controller;
-use App\Imports\ParticipantsImport;
-use App\Imports\TrxTrainingsImport;
 use App\Models\UploadLogModel;
 use App\Models\TrxResultsModel;
 use App\Helpers\CleanerHelper;
-use Maatwebsite\Excel\Facades\Excel;
-use PHPHtmlParser\Dom;
 
 class UploadController extends Controller
 {
@@ -43,148 +40,76 @@ class UploadController extends Controller
 
     public function fileUploadParticipants(UploadParticipantsRequest $request)
     {
-        $cleaner = new CleanerHelper();
+        $rawfile = $request->file('data_participants');
 
-        $idata = Excel::toArray(new ParticipantsImport, $file);
-        $title = $idata[0][1][0];
-        $ta = explode(' ', $cleaner->removeWhiteSpace($idata[0][2][0]));
-        $ta = $ta[2];
-
-        Excel::Import(new ParticipantsImport, $file);
-        Excel::Import(new TrxTrainingsImport, $file);
-
-        $this->saveUploadLog($this->changeFileName('data_participants'), $title, $ta, $this->email);
+        $this->saveUploadLog($this->changeFileName($rawfile), $title, $ta, $this->email);
 
         return redirect('upload')->with('status', 'Successfully uploaded!');
     }
 
     public function fileUploadResults(UploadResultsRequest $request) {
+        $c = New CleanerHelper();
+
         $rawfile = $request->file('data_results');
-        // $tempfile = fopen('temp/temp.txt', 'w') or die('Unable to open file!');
-        $html = fopen($rawfile, 'r') or die('Unable to open file!');
+        // Create a CSV reader instance
+        $reader = Reader::createFromFileObject($rawfile->openFile());
+        $records = $reader->getRecords();
 
-        /* this code use python to do the html parse
-            fwrite($tempfile, fgets($html));
-            fclose($tempfile);
-            $cmd = 'python bin/synrs.py 2>&1';
-
-            try {
-                $exe = shell_exec($cmd);
-            } catch (\Exception $e) {
-                return $e->getMessage();
-            }
-
-            $json = json_decode($exe, true);
-        */
-
-        $html = preg_replace('/( xmlns:o="urn:schemas-microsoft-com:office:office")/', '', fgets($html));
-        $html = preg_replace('/( xmlns:x="urn:schemas-microsoft-com:office:excel")/', '', $html);
-        $html = preg_replace('/( xmlns="http:\/\/www.w3.org\/TR\/REC-html40")/', '', $html);
-        $html = preg_replace('/(<!--\[if gte mso 9\]>)/', '', $html);
-        $html = preg_replace('/(<!\[endif\]-->)/', '', $html);
-        $html = preg_replace('/(<meta charset="utf-8"\/>)/', '',$html);
-        $html = preg_replace('/(<xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>data<\/x:Name><x:WorksheetOptions><x:DisplayGridlines\/><\/x:WorksheetOptions><\/x:ExcelWorksheet><\/x:ExcelWorksheets><\/x:ExcelWorkbook><\/xml>)/', '', $html);
-
-        $dom = new \DOMDocument;
-        $html = $dom->loadHTML($html);
-        $tbody = $dom->getElementsByTagName('tbody');
-        $rows = $tbody->item(0)->getElementsByTagName('tr');
-
-        foreach ($rows as $row) {
-            $cols = $row->getElementsByTagName('td');
-
-            // Fix weird case where date field is empty string...
-            $date = now(); // default
-            if (is_object($cols->item(42))) {
-                if(!empty($cols->item(42)->nodeValue)) {
-                    $date = $cols->item(42)->nodeValue;
-                } else {
-                    $date = now();
-                }
-            }
-
-            if(empty($date)) {
-                $date = now();
-            }
-            if($date == '-') {
-                $date = now();
-            }
-            if($date == '') {
-                $date = now();
-            }
-
-            $bdate = now(); // default
-            if (is_object($cols->item(19))) {
-                if(!empty($cols->item(19)->nodeValue)) {
-                    $bdate = $cols->item(19)->nodeValue;
-                } else {
-                    $bdate = now();
-                }
-            }
-            if(empty($date)) {
-                $bdate = now();
-            }
-            if($date == '-') {
-                $bdate = now();
-            }
-            if($date == '') {
-                $bdate = now();
-            }
-
-            $data = [
-                'training_id' => (is_object($cols->item(0)) ? $cols->item(0)->nodeValue : NULL),
-                'training_name' => (is_object($cols->item(1)) ? $cols->item(1)->nodeValue : NULL),
-                'start' => (is_object($cols->item(2)) ? $cols->item(2)->nodeValue : NULL),
-                'end' => (is_object($cols->item(3)) ? $cols->item(3)->nodeValue : NULL),
-                'hours' => (is_object($cols->item(4)) ? $cols->item(4)->nodeValue : NULL),
-                'days' => (is_object($cols->item(5)) ? $cols->item(5)->nodeValue : NULL),
-                'organization_name' => (is_object($cols->item(6)) ? $cols->item(6)->nodeValue : NULL),
-                'type' => (is_object($cols->item(7)) ? $cols->item(7)->nodeValue : NULL),
-                'cost' => (is_object($cols->item(8)) ? $cols->item(8)->nodeValue : NULL),
-                'cost_detail' => (is_object($cols->item(9)) ? $cols->item(9)->nodeValue : NULL),
-                'elearning' => (is_object($cols->item(10)) ? $cols->item(10)->nodeValue : NULL),
-                'type_test' => (is_object($cols->item(11)) ? $cols->item(11)->nodeValue : NULL),
-                'class' => (is_object($cols->item(12)) ? $cols->item(12)->nodeValue : NULL),
-
-                'student_id' => (is_object($cols->item(13)) ? $cols->item(13)->nodeValue : NULL),
-                'name' => (is_object($cols->item(14)) ? $cols->item(14)->nodeValue : NULL),
-                'nip' => (is_object($cols->item(15)) ? preg_replace('/~/', '', $cols->item(15)->nodeValue) : NULL),
-                'nrp_nik' => (is_object($cols->item(16)) ? preg_replace('/~/', '', $cols->item(16)->nodeValue) : NULL),
-                'rank_class' => (is_object($cols->item(17)) ? $cols->item(17)->nodeValue : NULL),
-                'born' => (is_object($cols->item(18)) ? $cols->item(18)->nodeValue : NULL),
-                'birthday' => $bdate,
-                'gender' => (is_object($cols->item(20)) ? $cols->item(20)->nodeValue : NULL),
-                'phone' => (is_object($cols->item(21)) ? $cols->item(21)->nodeValue : NULL),
-                'email' => (is_object($cols->item(22)) ? $cols->item(22)->nodeValue : NULL),
-                'office_address' => (is_object($cols->item(23)) ? $cols->item(23)->nodeValue : NULL),
-                'education' => (is_object($cols->item(24)) ? $cols->item(24)->nodeValue : NULL),
-                'education_desc' => (is_object($cols->item(25)) ? $cols->item(25)->nodeValue : NULL),
-                'position' => (is_object($cols->item(26)) ? $cols->item(26)->nodeValue : NULL),
-                'position_desc' => (is_object($cols->item(27)) ? $cols->item(27)->nodeValue : NULL),
-                'married' => (is_object($cols->item(28)) ? $cols->item(28)->nodeValue : NULL),
-                'religion' => (is_object($cols->item(29)) ? $cols->item(29)->nodeValue : NULL),
-                'main_unit' => (is_object($cols->item(30)) ? $cols->item(30)->nodeValue : NULL),
-                'eselon2' => (is_object($cols->item(31)) ? $cols->item(31)->nodeValue : NULL),
-                'eselon3' => (is_object($cols->item(32)) ? $cols->item(32)->nodeValue : NULL),
-                'eselon4' => (is_object($cols->item(33)) ? $cols->item(33)->nodeValue : NULL),
-                'satker' => (is_object($cols->item(34)) ? $cols->item(34)->nodeValue : NULL),
-                'test_result' => (is_object($cols->item(35)) ? $cols->item(35)->nodeValue : NULL),
-                'graduate_status' => (is_object($cols->item(36)) ? $cols->item(36)->nodeValue : NULL),
-                'activity' => (is_object($cols->item(37)) ? $cols->item(37)->nodeValue : NULL),
-                'presence' => (is_object($cols->item(38)) ? $cols->item(38)->nodeValue : NULL),
-                'pre_test' => (is_object($cols->item(39)) ? $cols->item(39)->nodeValue : NULL),
-                'post_test' => (is_object($cols->item(40)) ? $cols->item(40)->nodeValue : NULL),
-
-                'number' => (is_object($cols->item(41)) ? $cols->item(41)->nodeValue : NULL),
-                'date' => $date,
-                'execution_value' => (is_object($cols->item(43)) ?  $cols->item(43)->nodeValue : NULL),
-                'trainer_value' => (is_object($cols->item(44)) ? $cols->item(44)->nodeValue : NULL)
+        foreach ($records as $offset => $record) {
+            $row = [
+                'training_id' => $record[0],
+                'training_name' => $record[1],
+                'start' => $record[2],
+                'end' => $record[3],
+                'hours' => $record[4],
+                'days' => $record[5],
+                'organization_name' => $record[6],
+                'type' => $record[7],
+                'cost' => $record[8],
+                'cost_detail' => $record[9],
+                'elearning' => $record[10],
+                'type_test' => $record[11],
+                'class' => $record[12],
+                'student_id' => $record[13],
+                'name' => $record[14],
+                'nip' => $c->removeWhiteSpace(ltrim($record[15], "~")),
+                'nrp_nik' => $c->removeWhiteSpace(ltrim($record[16], "~")),
+                'rank_class' => $record[17],
+                'born' => $record[18],
+                'birthday' => $record[19],
+                'gender' => $record[20],
+                'phone' => $record[21],
+                'email' => $record[22],
+                'office_address' => $record[23],
+                'office_phone' => $record[24],
+                'education' => $record[25],
+                'education_desc' => $record[26],
+                'position' => $record[27],
+                'position_desc' => $record[28],
+                'married' => $record[29],
+                'religion' => $record[30],
+                'main_unit' => $record[31],
+                'eselon2' => $record[32],
+                'eselon3' => $record[33],
+                'eselon4' => $record[34],
+                'satker' => $record[35],
+                'test_result' => $record[36],
+                'graduate_status' => $record[37],
+                'activity' => $record[38],
+                'presence' => $record[39],
+                'pre_test' => $record[40],
+                'post_test' => $record[41],
+                'number' => $record[42],
+                'date' => $record[43],
+                //'link' => $record[44],
+                'execution_value' => $record[45],
+                'trainer_value' => $record[46]
             ];
 
-            if(!empty(array_filter($data))) {
-                TrxResultsModel::create($data);
+            if(!empty(array_filter($row))) {
+                TrxResultsModel::create($row);
             }
-         }
+        }
 
         $this->saveUploadLog($this->changeFileName($rawfile), '', '', $this->email);
 
