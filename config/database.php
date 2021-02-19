35cc4d5046f066c973bb133d7875dbc223366214
Vitrio Wibisono <vitrio@noxus.co.id>
2021-02-19 09:23:05 +0700

    Added server database config

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,10 +68,10 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
+            'database' => env('DB_DATABASE', 'aposter'),
+            'username' => env('DB_USERNAME', 'use'),
+            'password' => env('DB_PASSWORD', 'user123'),
+            'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
