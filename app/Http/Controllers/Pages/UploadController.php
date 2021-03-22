b17d9fdc282b0fd53d28a609564395d23635e9c5
Vitrio Wibisono <vitrio@noxus.co.id>
2021-03-22 15:20:12 +0700

    Check csv delimiter

--- app/Http/Controllers/Pages/UploadController.php
+++ app/Http/Controllers/Pages/UploadController.php
@@ -103,17 +103,18 @@ class UploadController extends Controller
         return $fn;
     }
 
-    private function csvToArray($filename = '', $delimiter = ',')
+    private function csvToArray($filename = '')
     {
         if (!file_exists($filename) || !is_readable($filename))
             return false;
 
         $header = null;
         $data = [];
+        $delimiter = $this->csvDelimCheck($filename);
 
         if (($handle = fopen($filename, 'r')) !== false)
         {
-            while (($row = fgetcsv($handle, 9999, $delimiter)) !== false)
+            while (($row = fgetcsv($handle, 9999, max($delimiter))) !== false)
             {
                 if (!$header)
                     $header = $row;
@@ -128,6 +129,30 @@ class UploadController extends Controller
         }
 
         return $data;
+    }
+
+    private function csvDelimCheck($csvFile)
+    {
+        $delimiters = array( ',' => 0, ';' => 0, "\t" => 0, '|' => 0, );
+        $firstLine = '';
+        $handle = fopen($csvFile, 'r');
+
+        if ($handle)
+        {
+            $firstLine = fgets($handle); fclose($handle);
+        }
+        if ($firstLine)
+        {
+            foreach ($delimiters as $delimiter => &$count)
+            {
+                $count = count(str_getcsv($firstLine, $delimiter));
+            }
+            return(array_search(max($delimiters), $delimiters));
+        }
+        else
+        {
+            return(key($delimiters));
+        }
     }
 
     private function UTF8Conf($string)
