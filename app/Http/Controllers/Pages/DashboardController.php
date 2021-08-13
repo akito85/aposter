99bb302a2a9873f6ecb9bfc4caa366cfa67bd55b
vitrio wibisono <akito.evol@gmail.com>
2021-08-13 18:03:11 +0700

    Fixed dashboard

--- app/Http/Controllers/Pages/DashboardController.php
+++ app/Http/Controllers/Pages/DashboardController.php
@@ -135,7 +135,7 @@ class DashboardController extends Controller
         }
         else
         {
-            $evagara = '';
+            $evagara = json_encode([]);
         }
 
         $data = [
